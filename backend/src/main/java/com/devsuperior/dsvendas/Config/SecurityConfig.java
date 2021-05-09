f9fe4ae47bf99ea136d559ecb847c3118cdf1e94
Enoque Neres <enoque.sneres@live.com>
2021-05-09 17:56:30 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsvendas/Config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsvendas/Config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsvendas;
+package com.devsuperior.dsvendas.Config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
